3d2d53186a0cc5b911fe631f08ecd73d3587f77e
alexenferman <62704841+alexenferman@users.noreply.github.com>
2021-01-10 18:39:06 -0500

    Initial

---
+++ mediatek/custom/jrdsz72_we_lca/preloader/inc/custom_MemoryDevice.h
@@ -0,0 +1,14 @@
+
+
+#ifndef __CUSTOM_MEMORYDEVICE__
+#define __CUSTOM_MEMORYDEVICE__
+
+
+
+#define BOARD_ID                MT6572_EVB1
+
+#define CS_PART_NUMBER[0]       KMN5U000ZM_B203
+
+#define EMI_CLK                 266M
+
+#endif /* __CUSTOM_MEMORYDEVICE__ */
